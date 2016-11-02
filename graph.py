7886c0fe256ed42c25adc7b40cd11764d37197dd
Oliver Newman <oliver@whmsi.com>
2016-11-02 19:52:11 -0400

    Implement shortest_path with BFS

--- graph.py
+++ graph.py
@@ -3,15 +3,16 @@ Graph implementation in Python.
 Author: Oliver Newman
 Date: October 2016
 """
+from collections import deque
 
-class Graph(object):
+class Directed_Graph(object):
     """Implementation of an adjacency-list directed graph.
 
     Attributes:
         vertices: A dict mapping a vertex to a set of vertices to which that
             vertex is connected with an edge.
     """
-    def __init__(self, vertices = [], edges = []):
+    def __init__(self, vertices=None, edges=None):
         """Initializes a graph.
 
         Args:
@@ -21,10 +22,15 @@ class Graph(object):
                 with which to initialize the graph.
         """
         self.vertices = {}
-        for v in vertices:
-            self.add_vertex(v)
-        for e in edges:
-            self.add_edge(e[0], e[1])
+        if vertices is None:
+            return
+        for vert in vertices:
+            self.add_vertex(vert)
+
+        if edges is None:
+            return
+        for edge in edges:
+            self.add_edge(edge[0], edge[1])
 
     def __str__(self):
         """Returns a string representation of the Graph.
@@ -32,11 +38,15 @@ class Graph(object):
         Returns:
             The string representation of the Graph's vertices dict.
         """
-        result = "{"
         num_vertices = len(self.vertices)
-        for v in self.vertices:
-            result += "{}->{}, ".format(v, list(self.vertices[v]))
-        result += "\b\b}"
+        result = "{"
+
+        for i, vertex in enumerate(self.vertices):
+            result += "{}->{}".format(vertex, list(self.vertices[vertex]))
+            if i != num_vertices - 1:
+                result += ",\n"
+
+        result += "}"
         return result
 
     __repr__ = __str__
@@ -45,9 +55,9 @@ class Graph(object):
     def edges(self):
         """Returns a list of edges as 2-tuples of the form (start, end)."""
         edges = []
-        for v1 in self.vertices:
-            for v2 in self.vertices[v1]:
-                edges.append((v1, v2))
+        for start in self.vertices:
+            for end in self.vertices[start]:
+                edges.append((start, end))
         return edges
 
     def add_vertex(self, vertex):
@@ -60,7 +70,7 @@ class Graph(object):
 
     def add_edge(self, start, end):
         """Adds an edge from vertex start to vertex end.
-        
+
         Raises:
             KeyError if vertex named start or end does not exist.
         """
@@ -69,11 +79,16 @@ class Graph(object):
         else:
             raise KeyError("Can't attach edge to nonexistent vertex.")
 
+    def num_vertices(self):
+        """Returns the number of vertices in the graph."""
+        return len(self.vertices)
+
     def num_edges(self):
         """Returns the number of edges in the graph."""
         num_edges = 0
         for edges in self.vertices.values():
             num_edges += len(edges)
+        return num_edges
 
     def has_vertex(self, vertex):
         """Returns whether or not a vertex exists with the given name."""
@@ -93,12 +108,61 @@ class Graph(object):
         """Removes an edge from the graph."""
         self.vertices[start].remove(end)
 
-    def out_edges(self, vertex):
+    def children(self, vertex):
         """Returns a list of the outgoing edges from a vertex."""
         return list(self.vertices[vertex])
 
-    def in_edges(self, vertex):
+    def parents(self, vertex):
         """Returns a list of the incoming edges to a vertex."""
         return [v for v in self.vertices if vertex in self.vertices[v]]
 
-    # def is_reachable(self, start, end):
+    def shortest_path(self, start, end):
+        distance = {}
+        parent = {}
+        queue = deque()
+        for vertex in self.vertices:
+            distance[vertex] = -1
+            parent[vertex] = None
+
+        queue.appendleft(start)
+        distance[start] = 0
+        while len(queue):
+            curr = queue.pop()
+            if curr == end: # Found end vertex
+                path = [curr]
+                while parent[curr]:
+                    curr = parent[curr]
+                    path.insert(0, curr)
+                return path
+            for child in self.children(curr):
+                if distance[child] < 0:
+                    queue.appendleft(child)
+                    parent[child] = curr
+                    distance[child] = distance[curr] + 1 
+        return None
+
+    def is_reachable(self, start, end):
+        return bool(self.shortest_path(start, end))
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- test_graph.py
+++ test_graph.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 """
-Units tests for the graph module.
+Unit tests for the directed graph module.
 Author: Oliver Newman
 Date: October 2016
 """
 
 import unittest
-from graph import Graph
+from graph import Directed_Graph
 
 VC = 100 # Default vertex count
 VERTICES = list(range(VC))
-EDGES = [((37 * i + 4597820) % VC, (31 * i + 6324957) % VC) for i in range(VC**2)]
+EDGES = [((37 * i + 45978) % VC, (31 * i + 63249) % VC) for i in range(VC**2)]
+with open('state_borders.txt', 'r') as border_file:
+    STATE_BORDERS = [border.strip("\n").split(" ") for border in border_file.readlines()]
+    border_file.close()
 
 
 class TestGraphMethods(unittest.TestCase):
@@ -18,21 +21,54 @@ class TestGraphMethods(unittest.TestCase):
         graph = Graph(VERTICES)
         for v in VERTICES:
             self.assertTrue(graph.has_vertex(v))
+        self.assertEqual(graph.num_vertices(), len(VERTICES))
 
     def test_remove_vertex(self):
         graph = Graph(VERTICES)
         for v in VERTICES:
             graph.remove_vertex(v)
             self.assertFalse(graph.has_vertex(v))
+        self.assertEqual(graph.num_vertices(), 0)
 
     def test_add_edge(self):
         graph = Graph(VERTICES)
         for v in VERTICES:
-            for v in VERTICES:
-                graph.add_edge(v, (v + 3) % len(VERTICES))
+            dest1 = (v + 3) % len(VERTICES)
+            dest2 = (v + 8) % len(VERTICES)
+            graph.add_edge(v, dest1)
+            graph.add_edge(v, dest2)
+            graph.add_edge(dest1, v)
+            graph.add_edge(dest2, v)
         for v in VERTICES:
-            for v in VERTICES:
-                self.assertTrue(graph.has_edge(v, (v + 3) % len(VERTICES)))
+            dest1 = (v + 3) % len(VERTICES)
+            dest2 = (v + 8) % len(VERTICES)
+            self.assertTrue(graph.has_edge(v, dest1))
+            self.assertTrue(graph.has_edge(v, dest2))
+        self.assertEqual(graph.num_edges(), 4 * len(VERTICES))
+
+    def test_shortest_path(self):
+        graph = Graph(VERTICES)
+        for v in VERTICES:
+            dest1 = (v + 3) % len(VERTICES)
+            dest2 = (v + 8) % len(VERTICES)
+            graph.add_edge(v, dest1)
+            graph.add_edge(v, dest2)
+            # graph.add_edge(dest1, v)
+            # graph.add_edge(dest2, v)
+        for v in VERTICES:
+            print(graph.shortest_path(v, VERTICES[0]))
+    
+    def test_states(self):
+        graph = Graph()
+        for border in STATE_BORDERS:
+            for state in border:
+                try:
+                    graph.add_vertex(state)
+                except ValueError:
+                    pass
+            graph.add_edge(*border)
+            graph.add_edge(*reversed(border))
+        print(graph.shortest_path("ME", "CA"))
 
 
 if __name__ == "__main__":
